0d479761bf38c9aebd05d60de2d358b3cd6bb7b4
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-07-03 09:21:04 +0200

    added setup.py making this an installable Python package

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    import pypandoc
+    LDESC = open('README.md', 'r').read()
+    LDESC = pypandoc.convert(LDESC, 'rst', format='md')
+except (ImportError, IOError, RuntimeError) as e:
+    print("Pandoc / pypandoc missing - couldn't create convert README.md to reStructuredText")
+    print(str(e))
+    LDESC = ''
+
+setup(name='docker-template',
+      version = '1.dev',
+      description = 'docker-template = A tool to rendering Jinja2 templates to Dockerfiles',
+      long_description = LDESC,
+      author = 'Philipp Klaus',
+      author_email = 'philipp.l.klaus@web.de',
+      url = 'https://github.com/pklaus/docker-template',
+      license = 'GPL',
+      #packages = ['',],
+      py_modules = ['docker_template',],
+      entry_points = {
+          'console_scripts': [
+              'docker-template = docker_template:main',
+          ],
+      },
+      zip_safe = True,
+      platforms = 'any',
+      install_requires = [
+          "jinja2",
+      ],
+      keywords = 'PySerial Manager',
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Archiving :: Packaging',
+      ]
+)
